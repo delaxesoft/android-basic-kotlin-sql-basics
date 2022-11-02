9eb83d8a420530a12d4c64b38f5db25f11b1d99a
delax <delaxesoft@gmail.com>
2022-11-02 13:16:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android_basic_kotlin_sql_basics/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.android_basic_kotlin_sql_basics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            AppDatabase.getDatabase(applicationContext).californiaParkDao().getAll()
+        }
+    }
+}
